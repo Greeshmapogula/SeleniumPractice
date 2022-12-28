0f4e74c9bb505e1a3a5f64105f0d52cf2f5b972c
POGULA SAI LAKSHMI PRASANNA GREESHMA <greeshmapogula@gmail.com>
2022-12-28 10:32:16 +0530

    Add files via upload

---
+++ LaunchBrowser.java
@@ -0,0 +1,18 @@
+package github.com.SeliniumPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class LaunchBrowser {
+	public static void main(String[] args) throws Exception {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		
+	}
+
+}
